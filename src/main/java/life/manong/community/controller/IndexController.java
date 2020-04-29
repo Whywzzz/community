21829172db7918bd81a60111c19e3c84a8cf5ad0
wzzz <cheerwz@163.com>
2020-04-29 22:24:00 +0800

    add check cookies

--- src/main/java/life/manong/community/controller/IndexController.java
+++ src/main/java/life/manong/community/controller/IndexController.java
@@ -1,14 +1,34 @@
 package life.manong.community.controller;
 
+import life.manong.community.mapper.UserMapper;
+import life.manong.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
-        return "index";//访问名为hello的html页面
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+        return "index";//index
     }
 }
--- src/main/java/life/manong/community/mapper/UserMapper.java
+++ src/main/java/life/manong/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package life.manong.community.mapper;
 import life.manong.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_Create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
